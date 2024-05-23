42aa3b02398b82e708cc938c15f482cc3b11947c
pperjar <pedro1971val@gmail.com>
2024-05-23 20:31:32 -0300

    Update script.js

--- Exercicio 03-f/script.js
+++ Exercicio 03-f/script.js
@@ -3,13 +3,14 @@ let valor1 = Number (prompt("Informe o valor1"));
 let valor2 = Number (prompt("Informe o valor2"));
 let valor3 = Number (prompt("Informe o valor3"));
 let menor,maior,meio;
-if ((valor1 < valor2)&&(valor1 < valor3)){
-menor=valor1;
+if((valor1 < valor2)&&(valor1 < valor3)){
+menor = valor1
 }
-if (valor2<valor3){
+if(valor2 < valor3) {
 meio= valor2
-maior= valor3
-}else{meio=valor3
-maior=valor2
+maior = valor3 
+} else {
+meio= valor3
+maior = valor2
 }
-alert (`${menor} $ {meio} $ {maior}`);
+alert (`${menor} ${meio} ${maior}`);
