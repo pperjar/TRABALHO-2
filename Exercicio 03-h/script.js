1bc02eeb1a55b090ebb283dccd29eb48b483f085
pperjar <pedro1971val@gmail.com>
2024-05-24 21:41:37 -0300

    Add files via upload

---
+++ Exercicio 03-h/script.js
@@ -0,0 +1,22 @@
+alert("Bem-vindo!"); 
+let valor1= Number(prompt("informe o valor1"));
+let valor2= Number(prompt("informe o valor2"));
+let valor3= Number(prompt("informe o valor3"));
+let valor4= Number(prompt("informe o valor4"));
+let resultado=[];
+if(valor1%2==0 || valor1%3==0){
+resultado.push(valor1);
+}
+if(valor2%2==0 || valor2%3==0){
+resultado.push(valor2);
+}
+if(valor3%2==0 || valor3%3==0){
+resultado.push(valor3);
+}
+if(valor4%2==0 || valor4%3==0){
+resultado.push(valor4);
+}
+if(resultado.length<1){
+alert("valor invalido");
+}
+alert(resultado);
