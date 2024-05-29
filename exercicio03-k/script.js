c667cb78de82f8ab74cb118ae9b6da9f9d6c008e
pperjar <pedro1971val@gmail.com>
2024-05-28 21:21:35 -0300

    Add files via upload

---
+++ exercicio03-k/script.js
@@ -0,0 +1,10 @@
+alert("Bem-vindo!"); 
+let valor = parseInt(prompt("Digite um numero inteiro qualquer de 1 e 9:"));
+
+if (valor > 1  || valor < 9) {
+alert("o valor está na faixa permitida")
+} else {
+alert ("o valor está fora da faixa permitida")
+}
+
+
