f787d4fdafd5f87ee37f76e1a8cc3ebed2924cad
pperjar <pedro1971val@gmail.com>
2024-05-28 21:27:16 -0300

    Add files via upload

---
+++ exercicio 03-m/script.js
@@ -0,0 +1,9 @@
+alert("Bem-vindo!"); 
+let nome = prompt("qual o seu nome?");
+let sexo = prompt("Digite o sexo (masculino/feminino");
+if (sexo.toLowerCase() === "masculino") {
+alert (`ilm.sr ${nome}`);
+} else {(sexo.toLowerCase() === "feminino") 
+alert (`ilma.sra ${nome}`);
+}
+
